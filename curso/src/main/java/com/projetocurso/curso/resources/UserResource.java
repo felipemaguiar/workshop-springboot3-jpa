5140818cfb9c6f256c219711d7edd07f577608cd
Felipe Magalhães <felipemaguiar@gmail.com>
2023-09-12 15:27:08 -0300

    User entity and resource

---
+++ curso/src/main/java/com/projetocurso/curso/resources/UserResource.java
@@ -0,0 +1,20 @@
+package com.projetocurso.curso.resources;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.projetocurso.curso.entities.User;
+
+@RestController
+@RequestMapping(value = "/users")
+public class UserResource {
+	
+	@GetMapping
+	public ResponseEntity<User> findAll(){
+		User u = new User(1L, "Felipe", "felip@gmail.com", "123", "123456");
+		return ResponseEntity.ok().body(u);
+	}
+
+}
