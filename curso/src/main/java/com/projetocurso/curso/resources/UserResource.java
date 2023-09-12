d27afad9d949ec5a7bc0f977bf7cce52e80ad8e2
Felipe Magalhães <felipemaguiar@gmail.com>
2023-09-12 15:52:06 -0300

    H2 database, test profile, JPA

--- curso/src/main/java/com/projetocurso/curso/resources/UserResource.java
+++ curso/src/main/java/com/projetocurso/curso/resources/UserResource.java
@@ -7,12 +7,13 @@ import org.springframework.web.bind.annotation.RestController;
 
 import com.projetocurso.curso.entities.User;
 
-@RestController
-@RequestMapping(value = "/users")
+@RestController // implementar o controlador rest
+@RequestMapping(value = "/users") // preciso dar um nome ao meu recurso
 public class UserResource {
 	
 	@GetMapping
-	public ResponseEntity<User> findAll(){
+	public ResponseEntity<User> findAll(){ //ResponseEntity - retorna resposta de 
+		//requisições web
 		User u = new User(1L, "Felipe", "felip@gmail.com", "123", "123456");
 		return ResponseEntity.ok().body(u);
 	}
